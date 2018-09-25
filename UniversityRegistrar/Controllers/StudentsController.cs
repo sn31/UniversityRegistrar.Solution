73f12d8f12c11d4c72a70896de5bcaaf3d492332
Skye Nguyen and Vera Weikel <skye@>
2018-09-25 14:56:03 -0700

    add create form for new students

--- UniversityRegistrar/Controllers/StudentsController.cs
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -5,14 +5,31 @@ using UniversityRegistrar.Models;
 
 namespace UniversityRegistrar.Controllers
 {
-    public class StudentsController:Controller{
+    public class StudentsController : Controller
+    {
         [HttpGet("/students")]
         public ActionResult Index()
         {
-            Dictionary<string,object> model = new Dictionary<string,object>{};
-            List <Student> allStudents = Student.GetAll();
-            model.Add("students",allStudents);
+            Dictionary<string, object> model = new Dictionary<string, object> { };
+            List<Student> allStudents = Student.GetAll();
+            model.Add("students", allStudents);
             return View(model);
+        }
+
+        [HttpGet("/students/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/students")]
+        public ActionResult Create()
+        {
+            string newName = Request.Form["newName"];
+            DateTime newDate = Convert.ToDateTime(Request.Form["newDate"]);
+            Student newStudent = new Student(newName, newDate);
+            newStudent.Save();
+            return RedirectToAction("Index");
         }
     }
 }

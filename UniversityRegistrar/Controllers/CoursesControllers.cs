c61b7bbb578825db36fa16ae475a4ba398996be7
Skye Nguyen and Vera Weikel <skye@>
2018-09-25 16:09:46 -0700

    add update and delete to courses views and controllers.

--- UniversityRegistrar/Controllers/CoursesControllers.cs
+++ UniversityRegistrar/Controllers/CoursesControllers.cs
@@ -12,7 +12,7 @@ namespace UniversityRegistrar.Controllers
         {
             Dictionary<string, object> model = new Dictionary<string, object> { };
             List<Course> allCourses = Course.GetAll();
-            List<Student> allStudents = Student.GetAll();
+            List<Course> allStudents = Student.GetAll();
             model.Add("courses", allCourses);
             model.Add("students", allStudents);
             return View(model);
@@ -41,5 +41,19 @@ namespace UniversityRegistrar.Controllers
         //     Student newStudent = Student.Find(courseId);
         //     return  RedirectToAction("Index");
         // }
+
+        [HttpPost("/courses/delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            Course foundCourse = Course.Find(id);
+
+            foundCourse.Delete();
+            return RedirectToAction("Index");
+        }
+        [HttpGet("/courses/update")]
+        public ActionResult UpdateForm()
+        {
+            return View();
+        }
     }
 }
